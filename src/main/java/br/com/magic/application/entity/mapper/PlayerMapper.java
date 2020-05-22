db065a2f2aa638c9548a425ea3fcf83b620bf931
SantosVictor1 <victor.udi10@hotmail.com>
2020-05-22 20:18:51 -0300

    Added new attributes into player and created its mapper

--- src/main/java/br/com/magic/application/entity/mapper/PlayerMapper.java
+++ src/main/java/br/com/magic/application/entity/mapper/PlayerMapper.java
@@ -1,4 +1,15 @@
 package br.com.magic.application.entity.mapper;
 
+import br.com.magic.application.api.request.PlayerRequest;
+import br.com.magic.application.api.response.PlayerResponse;
+import br.com.magic.application.entity.dto.PlayerDTO;
+import br.com.magic.application.entity.model.Player;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface PlayerMapper {
+    PlayerDTO toDto(PlayerRequest playerRequest);
+    PlayerDTO toDto(Player player);
+    Player toEntity(PlayerDTO playerDTO);
+    PlayerResponse toResponse(PlayerDTO playerDTO);
 }
