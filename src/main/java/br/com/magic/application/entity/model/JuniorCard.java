4368e0b0f84cf00ae76d110d4ef6144d00040fcb
SantosVictor1 <victor.udi10@hotmail.com>
2020-05-22 19:41:06 -0300

    Added mapStruct and removed lombok

--- src/main/java/br/com/magic/application/entity/model/JuniorCard.java
+++ src/main/java/br/com/magic/application/entity/model/JuniorCard.java
@@ -5,15 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "junior_card")
-@NoArgsConstructor
-@Data
-@EqualsAndHashCode(callSuper = false)
 public class JuniorCard extends Card {
     @Column(name = "passive")
     private Integer passive;
@@ -21,4 +15,29 @@ public class JuniorCard extends Card {
     @ManyToOne(targetEntity = Player.class)
     @JoinColumn(name = "player_id", nullable = true)
     private Player player;
+
+    public JuniorCard() {
+    }
+
+    public JuniorCard(Long id, String title, String description, Integer cost, Integer lifeDamage, Integer passive, Player player) {
+        super(id, title, description, cost, lifeDamage);
+        this.passive = passive;
+        this.player = player;
+    }
+
+    public Integer getPassive() {
+        return passive;
+    }
+
+    public void setPassive(Integer passive) {
+        this.passive = passive;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
 }
--- src/main/java/br/com/magic/application/entity/model/Player.java
+++ src/main/java/br/com/magic/application/entity/model/Player.java
@@ -6,11 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
 @Entity
 @Table(name = "player")
 public class Player {
@@ -21,4 +17,28 @@ public class Player {
 
     @Column(name = "nick_name", nullable = false)
     private String nickName;
+
+    public Player() {
+    }
+
+    public Player(Long id, String nickName) {
+        this.id = id;
+        this.nickName = nickName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
 }
