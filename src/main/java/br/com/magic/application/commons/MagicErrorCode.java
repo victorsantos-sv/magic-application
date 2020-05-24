b1a1e503c521b59424b9ff3943f44f8add611377
SantosVictor1 <victor.udi10@hotmail.com>
2020-05-24 17:26:02 -0300

    Created errorCode and resourceBundle

---
+++ src/main/java/br/com/magic/application/commons/MagicErrorCode.java
@@ -0,0 +1,11 @@
+package br.com.magic.application.commons;
+
+public class MagicErrorCode {
+    private String code;
+    private String key;
+
+    public MagicErrorCode(String code, String key) {
+        this.code = code;
+        this.key = key;
+    }
+}
