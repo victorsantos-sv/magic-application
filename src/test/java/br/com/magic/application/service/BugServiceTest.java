ea44c7bd11a896e56ba3fa860e6571ad9b9848b2
SantosVictor1 <victor.udi10@hotmail.com>
2020-05-27 14:25:42 -0300

    Created test for BugService

---
+++ src/test/java/br/com/magic/application/service/BugServiceTest.java
@@ -0,0 +1,117 @@
+package br.com.magic.application.service;
+
+import br.com.magic.application.commons.MagicErrorCode;
+import br.com.magic.application.entity.dto.BugCardDTO;
+import br.com.magic.application.entity.dto.BugDTO;
+import br.com.magic.application.entity.dto.BugWithCardsDTO;
+import br.com.magic.application.entity.mapper.BugMapper;
+import br.com.magic.application.entity.model.Bug;
+import br.com.magic.application.exception.BugNotFound;
+import br.com.magic.application.repositories.BugRepositorie;
+import br.com.magic.application.services.IBugCardService;
+import br.com.magic.application.services.impl.BugService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Optional;
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+public class BugServiceTest {
+
+    private BugRepositorie bugRepositorie = Mockito.mock(BugRepositorie.class);
+    private IBugCardService bugCardService = Mockito.mock(IBugCardService.class);
+    private BugMapper bugMapper = Mockito.mock(BugMapper.class);
+    private BugService bugService = new BugService(bugRepositorie, bugCardService, bugMapper);
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    public void shouldLoadCardsAndCreateBug() throws IOException {
+        List<BugCardDTO> bugCardDTOList = buildBugCardsInUse();
+        Bug bug = new Bug();
+        bug.setId(1L);
+        BugWithCardsDTO bugWithCardsDTO = buildBugWithCardsDTO(bug, bugCardDTOList);
+        ArgumentCaptor<Bug> argumentCaptor = ArgumentCaptor.forClass(Bug.class);
+
+        Mockito.when(bugCardService.setCardsOnBug()).thenReturn(bugCardDTOList);
+        Mockito.when(bugRepositorie.save(Mockito.any())).thenReturn(bug);
+        Mockito.when(bugMapper.toDto(bug, bugCardDTOList)).thenReturn(bugWithCardsDTO);
+
+        BugWithCardsDTO bugWithCardsDTOLoaded = bugService.getInitialCards();
+
+        Assert.assertSame(bugWithCardsDTOLoaded, bugWithCardsDTO);
+
+        Mockito.verify(bugRepositorie, Mockito.times(1)).save(argumentCaptor.capture());
+        Mockito.verify(bugMapper, Mockito.times(1)).toDto(bug, bugCardDTOList);
+
+        Assert.assertSame(argumentCaptor.getValue().getLife(), new Bug().getLife());
+        Assert.assertSame(argumentCaptor.getValue().getMana(), new Bug().getMana());
+    }
+
+    @Test
+    public void shouldFindBugById() {
+        Long id = 1L;
+        Bug bug = new Bug(id, 20, 20);
+        BugDTO bugDTO = new BugDTO(id, 20, 20);
+
+        Mockito.when(bugRepositorie.findById(id)).thenReturn(Optional.of(bug));
+        Mockito.when(bugMapper.toDto(bug)).thenReturn(bugDTO);
+
+        BugDTO bugDTOFound = bugService.findById(id);
+
+        Assert.assertSame(bugDTOFound, bugDTO);
+
+        Mockito.verify(bugRepositorie, Mockito.times(1)).findById(id);
+        Mockito.verify(bugMapper, Mockito.times(1)).toDto(bug);
+    }
+
+    @Test
+    public void shouldThrowAnExceptionWhenNotFindBug() {
+        Long id = 1L;
+
+        Mockito.when(bugRepositorie.findById(id)).thenReturn(Optional.empty());
+
+        try {
+            bugService.findById(id);
+        } catch (BugNotFound ex) {
+            Assert.assertSame(ex.getCode(), MagicErrorCode.MEC005);
+        } finally {
+            Mockito.verify(bugRepositorie, Mockito.times(1)).findById(id);
+        }
+    }
+
+    @Test
+    public void shouldUpdateBug() {
+        BugDTO bugDTO = new BugDTO(1L, 15, 20);
+        Bug bug = new Bug(1L, 15, 20);
+
+        Mockito.when(bugMapper.toEntity(bugDTO)).thenReturn(bug);
+        Mockito.when(bugMapper.toDto(bug)).thenReturn(bugDTO);
+        Mockito.when(bugRepositorie.save(bug)).thenReturn(bug);
+
+        BugDTO bugDTOUpdated = bugService.updateBug(bugDTO);
+
+        Assert.assertSame(bugDTOUpdated, bugDTO);
+
+        Mockito.verify(bugMapper, Mockito.times(1)).toEntity(bugDTO);
+        Mockito.verify(bugMapper, Mockito.times(1)).toDto(bug);
+        Mockito.verify(bugRepositorie, Mockito.times(1)).save(bug);
+    }
+
+    private BugWithCardsDTO buildBugWithCardsDTO(Bug bug, List<BugCardDTO> bugCardDTOList) {
+        return new BugWithCardsDTO(bug.getId(), bug.getLife(), bug.getMana(), bugCardDTOList);
+    }
+
+    private List<BugCardDTO> buildBugCardsInUse() throws IOException {
+        String bugCardDTOListJson = IOUtils.toString(getClass().getClassLoader().
+            getResourceAsStream("payloads/bug-cards-dto-in-use.json"), Charset.forName("UTF-8"));
+
+        return objectMapper.readValue(bugCardDTOListJson, new TypeReference<List<BugCardDTO>>() {});
+    }
+}
--- src/test/java/br/com/magic/application/service/GameServiceTest.java
+++ src/test/java/br/com/magic/application/service/GameServiceTest.java
@@ -28,101 +28,98 @@ import org.mockito.Mockito;
 
 public class GameServiceTest {
 
-//    private IPlayerService playerService = Mockito.mock(IPlayerService.class);
-//    private IJuniorCardService juniorCardService = Mockito.mock(IJuniorCardService.class);
-//    private IBugCardService bugCardService = Mockito.mock(IBugCardService.class);
-//    private IBugService bugService = Mockito.mock(IBugService.class);
-//    private GameMapper mapper = Mockito.mock(GameMapper.class);
-//    private GameService gameService = new GameService(playerService, juniorCardService, bugCardService, bugService, mapper);
-//
-//    private final ObjectMapper objectMapper = new ObjectMapper();
-//
-//    @Test
-//    void shouldLoadBoardGame() throws IOException {
-//        Long id = 1L;
-//        PlayerDTO playerDTO = buildPlayerDTO(id);
-//        List<JuniorCardDTO> juniorCards = buildJuniorCardsList();
-//        BugDTO bugDTO = new BugDTO(1L, 20, 20, Collections.emptyList());
-//        PlayerWithCardsDTO playerWithCardsDTO = new PlayerWithCardsDTO(
-//            playerDTO.getId(),
-//            playerDTO.getNickName(),
-//            playerDTO.getLife(),
-//            playerDTO.getMana(),
-//            juniorCards
-//        );
-//        BugWithCardsDTO bugWithCardsDTO = new BugWithCardsDTO(
-//            bugDTO.getId(),
-//            bugDTO.getLife(),
-//            bugDTO.getMana(),
-//            bugDTO.getCards()
-//        );
-//        GameDTO gameDTO = buildGameDTO(playerWithCardsDTO, bugWithCardsDTO);
-//        ArgumentCaptor<List<JuniorCardDTO>> argumentCaptor = ArgumentCaptor.forClass(List.class);
-//
-//        Mockito.when(playerService.findById(id)).thenReturn(playerDTO);
-//        Mockito.when(juniorCardService.getCards()).thenReturn(juniorCards);
-//        Mockito.doNothing().when(juniorCardService).saveCardsIntoPlayer(Mockito.anyList(), Mockito.eq(id));
-//        Mockito.when(bugService.getInitialCards()).thenReturn(bugDTO);
-//        Mockito.when(mapper.toDto(Mockito.any(), Mockito.any())).thenReturn(gameDTO);
-//
-//        GameDTO gameDTOCreated = gameService.loadBoard(id);
-//
-//        Mockito.verify(playerService, Mockito.times(1)).findById(id);
-//        Mockito.verify(juniorCardService, Mockito.times(1)).getCards();
-//        Mockito.verify(juniorCardService, Mockito.times(1)).saveCardsIntoPlayer(argumentCaptor.capture(), Mockito.eq(id));
-//        Mockito.verify(playerService, Mockito.times(1)).findById(id);
-//
-//        Assert.assertSame(argumentCaptor.getValue().size(), 4);
-//        Assert.assertSame(gameDTOCreated.getBugWithCardsDTO().getId(), gameDTO.getBugWithCardsDTO().getId());
-//        Assert.assertSame(gameDTOCreated.getPlayerWithCardsDTO().getId(), gameDTO.getPlayerWithCardsDTO().getId());
-//    }
-//
-//    @Test
-//    void shouldLoadAllStackCards() throws IOException {
-//        List<JuniorCardDTO> juniorCards = buildStackJuniorCards();
-//        List<BugCardDTO> bugCards = buildStackBugCards();
-//
-//        Mockito.when(juniorCardService.getCardsWithoutPlayer()).thenReturn(juniorCards);
-//        Mockito.when(bugCardService.getCardsWithoutBug()).thenReturn(bugCards);
-//
-//        StackCardsDTO stackCardsDTO = gameService.getStackCards();
-//
-//        Assert.assertSame(stackCardsDTO.getBugCards(), bugCards);
-//        Assert.assertSame(stackCardsDTO.getJuniorCards(), juniorCards);
-//
-//        Mockito.verify(juniorCardService, Mockito.times(1)).getCardsWithoutPlayer();
-//        Mockito.verify(bugCardService, Mockito.times(1)).getCardsWithoutBug();
-//    }
-//
-//    private PlayerDTO buildPlayerDTO(Long id) {
-//        return new PlayerDTO(id, "player", 20, 20);
-//    }
-//
-//    private GameDTO buildGameDTO(PlayerWithCardsDTO playerWithCardsDTO, BugWithCardsDTO bugWithCardsDTO) {
-//        return new GameDTO(
-//            playerWithCardsDTO,
-//            bugWithCardsDTO
-//        );
-//    }
-//
-//    private List<JuniorCardDTO> buildStackJuniorCards() throws IOException {
-//        String juniorCardsJson = IOUtils.toString(getClass().getClassLoader()
-//            .getResourceAsStream("payloads/junior-cards-on-stack.json"), Charset.forName("UTF-8"));
-//
-//        return objectMapper.readValue(juniorCardsJson, new TypeReference<List<JuniorCardDTO>>() {});
-//    }
-//
-//    private List<BugCardDTO> buildStackBugCards() throws IOException {
-//        String juniorCardsJson = IOUtils.toString(getClass().getClassLoader()
-//            .getResourceAsStream("payloads/bug-cards-on-stack.json"), Charset.forName("UTF-8"));
-//
-//        return objectMapper.readValue(juniorCardsJson, new TypeReference<List<BugCardDTO>>() {});
-//    }
-//
-//    private List<JuniorCardDTO> buildJuniorCardsList() throws IOException {
-//        String juniorCardsJson = IOUtils.toString(getClass().getClassLoader().
-//            getResourceAsStream("payloads/junior-cards-dto-payload.json"), Charset.forName("UTF-8"));
-//
-//        return objectMapper.readValue(juniorCardsJson, new TypeReference<List<JuniorCardDTO>>(){});
-//    }
+    private IPlayerService playerService = Mockito.mock(IPlayerService.class);
+    private IJuniorCardService juniorCardService = Mockito.mock(IJuniorCardService.class);
+    private IBugCardService bugCardService = Mockito.mock(IBugCardService.class);
+    private IBugService bugService = Mockito.mock(IBugService.class);
+    private GameMapper mapper = Mockito.mock(GameMapper.class);
+    private GameService gameService = new GameService(playerService, juniorCardService, bugCardService, bugService, mapper);
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void shouldLoadBoardGame() throws IOException {
+        Long id = 1L;
+        PlayerDTO playerDTO = buildPlayerDTO(id);
+        List<JuniorCardDTO> juniorCards = buildJuniorCardsList();
+        PlayerWithCardsDTO playerWithCardsDTO = new PlayerWithCardsDTO(
+            playerDTO.getId(),
+            playerDTO.getNickName(),
+            playerDTO.getLife(),
+            playerDTO.getMana(),
+            juniorCards
+        );
+        BugWithCardsDTO bugWithCardsDTO = new BugWithCardsDTO(1L, 20, 20, Collections.emptyList());
+        GameDTO gameDTO = buildGameDTO(playerWithCardsDTO, bugWithCardsDTO);
+        ArgumentCaptor<List<JuniorCardDTO>> argumentCaptor = ArgumentCaptor.forClass(List.class);
+
+        Mockito.when(playerService.findById(id)).thenReturn(playerDTO);
+        Mockito.when(juniorCardService.getCards()).thenReturn(juniorCards);
+        Mockito.doNothing().when(juniorCardService).saveCardsIntoPlayer(Mockito.anyList(), Mockito.eq(id));
+        Mockito.when(bugService.getInitialCards()).thenReturn(bugWithCardsDTO);
+        Mockito.when(mapper.toDto(Mockito.any(), Mockito.any())).thenReturn(gameDTO);
+
+        GameDTO gameDTOCreated = gameService.loadBoard(id);
+
+        Mockito.verify(playerService, Mockito.times(1)).findById(id);
+        Mockito.verify(juniorCardService, Mockito.times(1)).getCards();
+        Mockito.verify(juniorCardService, Mockito.times(1)).saveCardsIntoPlayer(argumentCaptor.capture(), Mockito.eq(id));
+        Mockito.verify(playerService, Mockito.times(1)).findById(id);
+
+        Assert.assertSame(argumentCaptor.getValue().size(), 4);
+        Assert.assertSame(gameDTOCreated.getBugWithCardsDTO().getId(), gameDTO.getBugWithCardsDTO().getId());
+        Assert.assertSame(gameDTOCreated.getPlayerWithCardsDTO().getId(), gameDTO.getPlayerWithCardsDTO().getId());
+    }
+
+    @Test
+    void shouldLoadAllStackCards() throws IOException {
+        List<JuniorCardDTO> juniorCards = buildStackJuniorCards();
+        List<BugCardDTO> bugCards = buildStackBugCards();
+
+        Mockito.when(juniorCardService.getCardsWithoutPlayer()).thenReturn(juniorCards);
+        Mockito.when(bugCardService.getCardsWithoutBug()).thenReturn(bugCards);
+
+        StackCardsDTO stackCardsDTO = gameService.getStackCards();
+
+        Assert.assertSame(stackCardsDTO.getBugCards(), bugCards);
+        Assert.assertSame(stackCardsDTO.getJuniorCards(), juniorCards);
+
+        Mockito.verify(juniorCardService, Mockito.times(1)).getCardsWithoutPlayer();
+        Mockito.verify(bugCardService, Mockito.times(1)).getCardsWithoutBug();
+    }
+
+    @Test
+
+
+    private PlayerDTO buildPlayerDTO(Long id) {
+        return new PlayerDTO(id, "player", 20, 20);
+    }
+
+    private GameDTO buildGameDTO(PlayerWithCardsDTO playerWithCardsDTO, BugWithCardsDTO bugWithCardsDTO) {
+        return new GameDTO(
+            playerWithCardsDTO,
+            bugWithCardsDTO
+        );
+    }
+
+    private List<JuniorCardDTO> buildStackJuniorCards() throws IOException {
+        String juniorCardsJson = IOUtils.toString(getClass().getClassLoader()
+            .getResourceAsStream("payloads/junior-cards-on-stack.json"), Charset.forName("UTF-8"));
+
+        return objectMapper.readValue(juniorCardsJson, new TypeReference<List<JuniorCardDTO>>() {});
+    }
+
+    private List<BugCardDTO> buildStackBugCards() throws IOException {
+        String juniorCardsJson = IOUtils.toString(getClass().getClassLoader()
+            .getResourceAsStream("payloads/bug-cards-on-stack.json"), Charset.forName("UTF-8"));
+
+        return objectMapper.readValue(juniorCardsJson, new TypeReference<List<BugCardDTO>>() {});
+    }
+
+    private List<JuniorCardDTO> buildJuniorCardsList() throws IOException {
+        String juniorCardsJson = IOUtils.toString(getClass().getClassLoader().
+            getResourceAsStream("payloads/junior-cards-dto-payload.json"), Charset.forName("UTF-8"));
+
+        return objectMapper.readValue(juniorCardsJson, new TypeReference<List<JuniorCardDTO>>(){});
+    }
 }
